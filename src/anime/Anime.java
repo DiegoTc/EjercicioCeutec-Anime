595186a864ac1b0a4112c1fe7ed6f43e550e62bd
Docente <Docente@10.48.0.41>
2018-02-26 20:52:41 -0600

    Agregando ejemplo de expresiones regulares

--- src/anime/Anime.java
+++ src/anime/Anime.java
@@ -19,43 +19,80 @@ public class Anime {
     /**
      * @param args the command line arguments
      */
+    private static int arraySize = 3;
+    private static Animes arregloAnime[] = new Animes[arraySize];
+    private static Scanner lea = new Scanner(System.in);
+
+    private static boolean checkArraySize() {
+        for (int i = 0; i < arraySize; i++) {
+            if (arregloAnime[i] == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static int getArrayPosition(){
+        for (int i = 0; i < arraySize; i++) {
+            if (arregloAnime[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+
+    public static void opcionEstudios() {
+        if (checkArraySize() == true) {
+            String nombreanime = "";
+            String horarioanime = "";
+            String estudio = "";
+            System.out.println(" Menu de estudio");
+            lea.nextLine();
+            System.out.println("Ingrese nombre del Anime");
+            nombreanime = lea.nextLine();
+            System.out.println("Ingrese horario del Anime ---  (<inicio-fin>)||<16-20>");
+            horarioanime = lea.nextLine();
+            System.out.println("Ingrese Estudio creadora del anime");          
+            estudio = lea.nextLine();
+            Animes anime = new Animes(nombreanime, horarioanime, estudio);
+            int posicion = getArrayPosition();
+            arregloAnime[posicion] = anime;
+        } else {
+            System.out.println("Ya no hay horarios disponibles para su anime");
+        }
+
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         int opcion = 0;
-        Scanner lea = new Scanner(System.in);
-        String nombreanime="";
-        String horarioanime="";
-        String estudio="";
-
+        String horario = "16-av";
+        boolean a;
+        a = horario.matches("^\\d{2}-\\d{2}$");
         do {
             try {
                 System.out.println("Menu");
                 System.out.println("Opcion #1 --- Estudios");
-                System.out.println("Opcion #2 --- Televisonra");
+                System.out.println("Opcion #2 --- Televisora");
                 System.out.println("Opcion #3 --- Salir");
                 opcion = lea.nextInt();
-                
-                switch (opcion){
-                        case 1:
-                            System.out.println(" Menu de estudio");
-                            System.out.println("Ingrese nombre del Anime");
-                            System.out.println("Ingrese horario del Anime ---  (<inicio-fin>)||<16-20>");
-                            System.out.println("Ingrese Estudio creadora del anime");
-                            nombreanime = lea.nextLine();
-                            horarioanime = lea.nextLine();
-                            estudio = lea.nextLine();
-                            break;
-                        
-                        case 2:
-                            break;
-                        
-                        case 3:    
-                            break;
-                            
-                        default:
-                            System.out.println("Opcion no valida");
+
+                switch (opcion) {
+                    case 1:
+                        opcionEstudios();
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    default:
+                        System.out.println("Opcion no valida");
                 }
-                
+
             } catch (Exception ex) {
                 lea.nextLine();
                 System.out.println("Ingreso un valor no numerico");
